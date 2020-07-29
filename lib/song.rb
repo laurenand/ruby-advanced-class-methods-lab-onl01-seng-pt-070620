dee5ff5f783b01cae540b40ea02b76b278cc8f32
Lauren Anderson <laurenaanderson95@gmail.com>
2020-07-29 23:48:32 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -28,6 +28,7 @@ class Song
   def self.find_by_name(song_name)
     @@all.find{|song| song.name == song_name}
   end 
-  def self.find_or_create_by_name
+  def self.find_or_create_by_name(song_name)
+    
   end
 end
