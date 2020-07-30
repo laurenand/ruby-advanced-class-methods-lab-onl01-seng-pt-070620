72f19e350bb5a3ab1ebb7a67a321f7734e69adf7
Lauren Anderson <laurenaanderson95@gmail.com>
2020-07-30 00:42:41 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,10 +3,13 @@ require 'pry'
 class Song
   attr_accessor :name, :artist_name, :song
   @@all = []
+  def initialize(name = nil)
+    @name = name
+    save
+  end
   def self.all
     @@all
   end
-
   def save
     self.class.all << self
   end
