4aa48fdf7cf090535b1ff2478f979dd6dbd066bf
Lauren Anderson <laurenaanderson95@gmail.com>
2020-07-30 00:37:13 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -44,13 +44,7 @@ class Song
     song 
   end 
   def self.create_from_filename(filename)
-    new_file = filename.split(" - ")
-    artist_name = new_file[0]
-    song_name = new_file[1].gsub(".mp3","")
-    song = self.create
-    song.name = song_name
-    song.artist_name = artist_name
-    song
+    self.new_from_filename
   end 
   def self.destroy_all
   end
